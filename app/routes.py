3a720663fcebea745085a16f20a501658a0e696e
daniyalq114 <165241329+daniyalq114@users.noreply.github.com>
2024-05-09 13:25:54 +0800

    Updated routes.py with leaderboard func

--- app/routes.py
+++ app/routes.py
@@ -19,8 +19,13 @@ def submission_func():
 
 @flaskApp.route('/leaderboard')
 def leaderboard_func():
-    return render_template('leaderboard.html')
-
+    type = request.args.get('type')
+    if type == 'rank':
+        return jsonify(rank_data)
+    elif type == 'user':
+        return jsonify(user_data)
+    else:
+        return jsonify({'error': 'Invalid type'})
 
 @flaskApp.route('/profile')
 def profile_func():
@@ -64,3 +69,4 @@ def map_input_func():
 def submit():
     return render_template('listGroup.html')
 '''
+
