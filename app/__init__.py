20b95affb0d14d3934c6ff4946d740f886ee00c8
daniyalq114 <165241329+daniyalq114@users.noreply.github.com>
2024-05-18 11:29:55 +0800

    Updated __init__.py to redirect user to user profile

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, url_for
+from flask_login import LoginManager
+
+
 
 flaskApp = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(flaskApp)
+login_manager.login_view = 'login_func' 
 from config import Config 
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
