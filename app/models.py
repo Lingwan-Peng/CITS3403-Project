0e796f1ef37c6e82e3053e79dc6df85e3a50b08b
daniyalq114 <165241329+daniyalq114@users.noreply.github.com>
2024-05-18 11:53:41 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,31 +7,30 @@ from sqlalchemy.orm import relationship
 from app import db, flaskApp
 
 class User(db.Model):
-  user_id = db.Column(db.Integer, primary_key=True)
-  user_name = db.Column(db.String(120), nullable=False)
-  user_password = db.Column(db.String(20), nullable=False)
-  user_email = db.Column(db.String(120), nullable=False, unique=True)
-  posts = db.relationship('Post', back_populates='post_author')
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), nullable=False)
+    user_password = db.Column(db.String(20), nullable=False)
+    user_email = db.Column(db.String(120), nullable=False, unique=True)
+    posts = db.relationship('Post', back_populates='post_author')
 
 class Station(db.Model):
-  station_id = db.Column(db.Integer, primary_key=True)
-  station_name = db.Column(db.String(120), nullable=False)
-  station_postcode = db.Column(db.Integer)
-  station_phone_number = db.Column(db.String(20))
-  station_address = db.Column(db.String(255))
-  def __repr__(self):
-    return f"<GasStation {self.station_name}, address {self.station_address}>\n"
+    station_id = db.Column(db.Integer, primary_key=True)
+    station_name = db.Column(db.String(120), nullable=False)
+    station_postcode = db.Column(db.Integer)
+    station_phone_number = db.Column(db.String(20))
+    station_address = db.Column(db.String(255))
+    def __repr__(self):
+        return f"<GasStation {self.station_name}, address {self.station_address}>\n"
 
 class Post(db.Model):
-  post_id = db.Column(db.Integer, primary_key=True)
-  superior_post_id = db.Column(db.String(120))
-  post_title = db.Column(db.String(255), nullable=False)
-  post_content = db.Column(db.Text, nullable=False)
-  post_author_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-  post_author = relationship('User', back_populates ='posts')
+    post_id = db.Column(db.Integer, primary_key=True)
+    superior_post_id = db.Column(db.String(120))
+    post_title = db.Column(db.String(255), nullable=False)
+    post_content = db.Column(db.Text, nullable=False)
+    post_author_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    post_author = relationship('User', back_populates='posts')
 
-
-
-# creates the database tables -> only needs to be run once
+# Creates the database tables -> only needs to be run once
 with flaskApp.app_context():
-  db.create_all()
+    db.create_all()
+
