964b1cae436cc61bbc1fb1a49e853fffb50a7573
Lingwan-Peng <116148043+Lingwan-Peng@users.noreply.github.com>
2024-05-18 21:57:43 +0800

    sync User table with userProfile

--- app/models.py
+++ app/models.py
@@ -8,8 +8,12 @@ from app import db
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(120), nullable=False)
-    user_password = db.Column(db.String(20), nullable=False)
+    user_password_hash = db.Column(db.String(256), nullable=False)
     user_email = db.Column(db.String(120), nullable=False, unique=True)
+    user_phone = db.Column(db.Integer, nullable=False, unique=True)
+    user_dob = db.Column(db.Date, nullable=True) 
+    user_bio = db.Column(db.Text, nullable=True) 
+    
     posts = db.relationship('Post', back_populates='post_author')
 
 class Station(db.Model):
